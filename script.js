b843a29a5365fe3e9dead8d2ae8a571d131929c3
PauloRobertoRibeiro <ribeirorobertopaulo@gmail.com>
2025-05-07 22:16:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-
-document.getElementById("form-contato").addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-    emailjs.sendForm('service_dod41n7', 'template_ux4h87o', this)
-      .then(function() {
-        alert("Mensagem enviada com sucesso!");
-      }, function(error) {
-        alert("Erro ao enviar: " + error.text);
-      });
-  });
-  
-  
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+  }
   
